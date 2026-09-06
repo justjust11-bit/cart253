ebdafc43b8cec6d2552c6aaf545972b66d50293d
justjust11-bit <justine.cormier11@gmail.com>
2026-09-06 19:00:15 -0400

    turned into UFO in space, blue green bg

--- topics/hello-world/version-control-workflow/js/script.js
+++ topics/hello-world/version-control-workflow/js/script.js
@@ -13,8 +13,8 @@
  * Create a canvas, hides the cursor
 */
 function setup() {
-    // A 640x480 canvas
-    createCanvas(640, 640);
+    // A 840x840 canvas
+    createCanvas(840, 840);
 
     // Don't show the cursor
     noCursor();
@@ -26,11 +26,11 @@ function setup() {
 */
 function draw() {
     // Make the background black (specified as RGB)
-    background(0, 0, 0);
+    background(90, 0, 90);
 
     // Draw a pyramid
     // How many levels for the pyramid
-    const levels = 10;
+    const levels = 22;
     // Loop through every level (backwards)
     for (let level = levels; level > 0; level--) {
         // Draw this layer
@@ -38,11 +38,11 @@ function draw() {
         // Set the grey shade of the level based on its number
         // e.g. level 1 will get a shade of 10 (dark gray), 
         // level 10 will be 255(white)
-        const shade = map(level, 1, levels, 10, 255);
+        const shade = map(level, 1, levels, 0, 150);
         // No line around the levels
         noStroke();
         // Set the fill colour to our shade (RGB)
-        fill(shade, shade, 0);
+        fill(50, shade, 100);
         // Draw rectangles from the centre
         rectMode(CENTER);
         // Draw the rectangle in the centre of the canvas
@@ -57,9 +57,27 @@ function draw() {
     push();
     // No line around the shape
     noStroke();
-    // Make it red (RGB)
-    fill(255, 0, 0);
+ 
+    // Make it DARK (RGB)
+    fill(40, 20, 40);
     // Draw a 100x100 circle at the mouse position
-    ellipse(mouseX, mouseY, 100, 100);
+    // Spaceship width
+    ellipse(mouseX, mouseY, 340, 45);
+         //spaceship top and bottom (head and tail)
+    ellipse(mouseX, mouseY - 20, 130, 70);
+    //lights on ship, starting from the middle to the extremities
+    fill(100,100,100);
+    ellipse(mouseX, mouseY + 14, 9, 6);
+    ellipse(mouseX + 60, mouseY + 14, 9, 6);
+    ellipse(mouseX - 60, mouseY + 14, 9, 6);
+    ellipse(mouseX + 110, mouseY + 10, 8, 5);
+    ellipse(mouseX - 110, mouseY + 10, 8, 5);
+    ellipse(mouseX + 155, mouseY + 2, 7, 4);
+    ellipse(mouseX - 155, mouseY + 2, 7, 4);
+
+    //shine on cap
+    fill(40,30,60);
+    ellipse(mouseX + 30, mouseY - 40, 17, 10);
+   
     pop();
 }
