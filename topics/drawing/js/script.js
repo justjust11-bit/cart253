e34a86497b5b363a3792933cdaa6d1d323d02a14
justjust11-bit <justine.cormier11@gmail.com>
2026-09-17 16:42:04 -0400

    Implemented big red record

--- topics/drawing/js/script.js
+++ topics/drawing/js/script.js
@@ -12,7 +12,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(640, 640);
 }
 
 
@@ -20,5 +20,26 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-
+    background(150);
+    
+    push();
+    fill(255, 0, 0);
+    stroke(255);
+    ellipse(320, 320, 480)
+    pop();
+    
+    
+    push();
+    fill("white");
+    noStroke();
+    ellipse(320, 320, 140, 140);
+    pop();
+    
+    push();
+    fill("#000000");
+    noStroke();
+    ellipse(320, 320, 20, 20);
+    pop();
+    
+    
 }
