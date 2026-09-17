f7dd1508554a41b62c324c3b05721d52ccd51db9
justjust11-bit <justine.cormier11@gmail.com>
2026-09-17 16:16:19 -0400

    Added comments to the Blank Page project

--- topics/instructions/function-calls/js/script.js
+++ topics/instructions/function-calls/js/script.js
@@ -1,22 +1,32 @@
 /**
- * Function Calls
- * Justine Cormier
+ * The blank page
+ * Justine Cormier (but really, Pippin Barr)
  * 
- * Working on the function calls examples.
+ * An exploration of the existential angst of a novelist 
+ * who must sit down at their pink desk and confront the 
+ * abyss that is a blank page of paper
+ * 
+ * The program is non-interactive to convey the inability
+ * to get started on the project. Try typing! Just try it!
  */
 
 "use strict";
 
-
+/**
+ * Creates the canvas for our masterpiece
+ */
 function setup() {
-    // Once at the beginning of the program
+    // Create the cancas at a standard resolution
     createCanvas(640, 480); //wide, high
 }
 
-
+/**
+ * Draws the writers desktop and a blank piece of paper
+ */
 
 function draw() {
-    // Every frame
+    // The pink desktop
     background(255, 100, 100); 
-    rect(200,80,240,320); //x, y, wide, high
+    //The blank piece of paper
+    rect(200,80,240,320); //x, y,(initial spot) wide, high
 }
