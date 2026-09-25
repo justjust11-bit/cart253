6ffd1351937fd050395bbdf9460294e91e610c70
justjust11-bit <justine.cormier11@gmail.com>
2026-09-25 16:02:01 -0400

    Used variables to make the circle change colours with mouse

--- topics/variables/introducing-variables/js/script.js
+++ topics/variables/introducing-variables/js/script.js
@@ -11,13 +11,26 @@
  * Create a canvas
 */
 function setup() {
-
+    createCanvas(640, 640);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws a circle in the center of the canvas
 */
 function draw() {
-
+    background(0);
+    
+    //draw the circle
+    push();
+    //where you mouse is on the board
+    fill(mouseX, mouseY, 0);
+    noStroke();
+    //circle stays in the center ALWAYS
+    ellipse(width / 2, height / 2, mouseX, mouseY);
+    //ellipse is under the cursor
+    //if you want the mouse to only run on one axis, do ex. 200 mouseY
+    ellipse(mouseX, mouseY, 200, 200);
+    pop();
+    
 }
